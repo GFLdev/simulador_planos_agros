74c3f85b2f825100b9c6bbba7c1e1fa3b5258f55
Gabriel Franco Lourenço <gabriel.franco@gfldev.com>
2024-06-13 17:40:22 -0300

    Reformatacao do codigo (ESLint + Prettier)

--- src/components/SimuladorPlanosForm.vue
+++ src/components/SimuladorPlanosForm.vue
@@ -1,11 +1,18 @@
 <script setup>
-import {ref, toRefs} from 'vue'
+import { ref, toRefs } from 'vue'
 import {
-  PhBuilding, PhScroll, PhUser,
-  PhCalculator, PhMoney, PhUserMinus,
-  PhUserPlus, PhUsers, PhUsersFour,
-  PhUsersThree, PhUserGear
-} from "@phosphor-icons/vue"
+  PhBuilding,
+  PhScroll,
+  PhUser,
+  PhCalculator,
+  PhMoney,
+  PhUserMinus,
+  PhUserPlus,
+  PhUsers,
+  PhUsersFour,
+  PhUsersThree,
+  PhUserGear
+} from '@phosphor-icons/vue'
 
 const props = defineProps({
   products: {
@@ -22,7 +29,7 @@ const props = defineProps({
   }
 })
 
-const {products, aux, inst} = toRefs(props);
+const { products, aux, inst } = toRefs(props)
 
 const instList = Object.keys(inst.value)
 const prodsList = Object.keys(products.value)
@@ -47,7 +54,7 @@ function simAble(index) {
 }
 
 function isAble() {
-  return req.value.every(x => x)
+  return req.value.every((x) => x)
 }
 
 function haveSim() {
@@ -91,117 +98,92 @@ function simulate() {
   <div class="w-full flex flex-col align-center gap-4">
     <div class="w-full sm:w-96 flex flex-col justify-center">
       <v-select
-          :items="instList"
-          density="comfortable"
-          label="Instituição"
-          variant="underlined"
-          v-model="selInst"
-          @update:modelValue="simAble(0)"
-          :prepend-inner-icon="selInst ? null : PhBuilding"
+        :items="instList"
+        density="comfortable"
+        label="Instituição"
+        variant="underlined"
+        v-model="selInst"
+        @update:modelValue="simAble(0)"
+        :prepend-inner-icon="selInst ? null : PhBuilding"
       ></v-select>
       <v-select
-          :items="prodsList"
-          density="comfortable"
-          label="Tipo de Plano"
-          variant="underlined"
-          v-model="selPlano"
-          @update:modelValue="simAble(1)"
-          :prepend-inner-icon="PhScroll"
+        :items="prodsList"
+        density="comfortable"
+        label="Tipo de Plano"
+        variant="underlined"
+        v-model="selPlano"
+        @update:modelValue="simAble(1)"
+        :prepend-inner-icon="PhScroll"
       ></v-select>
       <v-select
-          :items="salGroupList"
-          density="comfortable"
-          label="Faixa salarial do grupo familiar"
-          variant="underlined"
-          v-model="selSalGroup"
-          @update:modelValue="simAble(2)"
-          :prepend-inner-icon="PhMoney"
+        :items="salGroupList"
+        density="comfortable"
+        label="Faixa salarial do grupo familiar"
+        variant="underlined"
+        v-model="selSalGroup"
+        @update:modelValue="simAble(2)"
+        :prepend-inner-icon="PhMoney"
       ></v-select>
       <v-select
-          :items="ageGroupList"
-          density="comfortable"
-          label="Faixa etária do titular"
-          variant="underlined"
-          v-model="selTitAgeGroup"
-          @update:modelValue="simAble(3)"
-          :prepend-inner-icon="PhUserGear"
+        :items="ageGroupList"
+        density="comfortable"
+        label="Faixa etária do titular"
+        variant="underlined"
+        v-model="selTitAgeGroup"
+        @update:modelValue="simAble(3)"
+        :prepend-inner-icon="PhUserGear"
       ></v-select>
       <v-select
-          v-for="index in benefCount"
-          :key="index"
-          :items="ageGroupList"
-          density="comfortable"
-          :label="'Faixa etária do beneficiário ' + index"
-          variant="underlined"
-          v-model="selBenefsAgeGroup[index]"
-          @update:modelValue="simAble(3 + index)"
-          :prepend-inner-icon="
-            index === 1 ? PhUser :
-            index === 2 ? PhUsers :
-            index === 3 ? PhUsersThree :
-            PhUsersFour"
+        v-for="index in benefCount"
+        :key="index"
+        :items="ageGroupList"
+        density="comfortable"
+        :label="'Faixa etária do beneficiário ' + index"
+        variant="underlined"
+        v-model="selBenefsAgeGroup[index]"
+        @update:modelValue="simAble(3 + index)"
+        :prepend-inner-icon="
+          index === 1 ? PhUser : index === 2 ? PhUsers : index === 3 ? PhUsersThree : PhUsersFour
+        "
       ></v-select>
     </div>
-    <div
-        class="
-          flex
-          flex-col
-          sm:flex-row
-          gap-4
-          w-fit
-          justify-center"
-    >
-      <v-btn
-          variant="outlined"
-          @click="incrementBenef"
-          :prepend-icon="PhUserPlus"
-      >
+    <div class="flex flex-col sm:flex-row gap-4 w-fit justify-center">
+      <v-btn variant="outlined" @click="incrementBenef" :prepend-icon="PhUserPlus">
         Adicionar beneficiário
       </v-btn>
       <v-btn
-          variant="outlined"
-          @click="decrementBenef"
-          :disabled="benefCount === 0"
-          :prepend-icon="PhUserMinus"
+        variant="outlined"
+        @click="decrementBenef"
+        :disabled="benefCount === 0"
+        :prepend-icon="PhUserMinus"
       >
         Remover beneficiário
       </v-btn>
     </div>
     <div class="w-fit">
       <v-btn
-          variant="outlined"
-          @click="simulate"
-          :disabled="!isAble()"
-          :prepend-icon="PhCalculator"
+        variant="outlined"
+        @click="simulate"
+        :disabled="!isAble()"
+        :prepend-icon="PhCalculator"
       >
         Simular
       </v-btn>
     </div>
-    <v-divider
-        class="
-          w-full
-          px-8
-          border-black
-          mt-4"
-        :opacity="0.4"
-    ></v-divider>
+    <v-divider class="w-full px-8 border-black mt-4" :opacity="0.4"></v-divider>
     <div>
-      <v-list
-          lines="one"
-          density="comfortable"
-          v-if="isAble() && haveSim()"
-      >
+      <v-list lines="one" density="comfortable" v-if="isAble() && haveSim()">
         <v-list-item
-            title="Contribuição Bruta do Grupo Familiar"
-            :subtitle="'R$ ' + grossContrib.toFixed(2)"
+          title="Contribuição Bruta do Grupo Familiar"
+          :subtitle="'R$ ' + grossContrib.toFixed(2)"
         ></v-list-item>
         <v-list-item
-            title="Auxílio Suplementar do Grupo Familiar"
-            :subtitle="'R$ ' + auxTotal.toFixed(2)"
+          title="Auxílio Suplementar do Grupo Familiar"
+          :subtitle="'R$ ' + auxTotal.toFixed(2)"
         ></v-list-item>
         <v-list-item
-            title="Contribuição Devida do Grupo Familiar"
-            :subtitle="'R$ ' + liqContrib.toFixed(2)"
+          title="Contribuição Devida do Grupo Familiar"
+          :subtitle="'R$ ' + liqContrib.toFixed(2)"
         ></v-list-item>
       </v-list>
       <p v-if="!haveSim() || !isAble()">Selecione todos os campos</p>
@@ -209,6 +191,4 @@ function simulate() {
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
